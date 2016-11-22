61a7946a622652599ea457f2b094e53ff649ef7d
Michael Timbrook <timbrook480@gmail.com>
2016-11-22 16:32:34 -0500

    SSL Gen

---
+++ certs/gen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p /etc/ssl/sse.rit.edu/
+
+openssl req \
+  -new \
+  -newkey rsa:2048 \
+  -sha1 \
+  -days 365 \
+  -nodes \
+  -x509 \
+  -keyout /app/certs/server.key \
+  -out /app/certs/server.crt \
+  -config /app/certs/openssl.conf
+
+cat server.key server.crt > cert.pem
